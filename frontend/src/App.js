91cf03b52ae552641308b22def798280b22eb0f9
ethanial1 <camiloeiz00@gmail.com>
2021-08-26 19:09:10 -0500

    Crear y eliminar usuarios.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,12 @@ function App() {
     <Router>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous"></link>
       <Navigation/>
-      <Route path="/" exact component={NotesList}/>
-      <Route path="/edit/:id" component={CreateNoteForme}/>
-      <Route path="/create" component={CreateNoteForme}/>
-      <Route path="/user" component={CreateUser}/>
+      <div className="container p-4">
+        <Route path="/" exact component={NotesList}/>
+        <Route path="/edit/:id" component={CreateNoteForme}/>
+        <Route path="/create" component={CreateNoteForme}/>
+        <Route path="/user" component={CreateUser}/>
+      </div>
     </Router>
   );
 }
--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -4,27 +4,67 @@ import axios from 'axios';
 export default class CreateUser extends Component {
 
     state = {
-        users: []
+        users: [],
+        username: ''
     }
 
     async componentDidMount(){
+        this.getUsers();
+    }
+
+    getUsers = async () =>{
         // hacemos una petición con axios
         const res = await axios.get('http://localhost:4000/api/users');
         this.setState({users: res.data});
-        console.log(res.data);
+    }
+
+    onChangeUserName = (e) => {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onSumbit = async e => {
+        // Evitamos que el formulario resetee la página
+        e.preventDefault();
+
+        const res = await axios.post('http://localhost:4000/api/users', {
+            username: this.state.username
+        })
+        this.setState({username: ''});
+        this.getUsers();
+        
+    }
+
+    deleteUser = async (id) => {
+        const res = await axios.delete('http://localhost:4000/api/users/' + id);
+        console.log(res);
+        this.getUsers();
 
     }
 
     render() {
         return (
-            <div className="row container mt-5">
+            <div className="row">
                 <div className="col-md-4">
-                    form user                    
+                    <div className="card card-body">
+                        <h5>Crear nuevo Usuario</h5>
+                        <form onSubmit={this.onSumbit}>
+                            <div className="form-group">
+                                <input type="text" className="form-control" onChange={this.onChangeUserName} value={this.state.username}/>
+                            </div>
+                            <button type="submit" className="btn btn-success mt-2">save</button>
+                        </form>
+                    </div>                
                 </div>
                 <div className="col-md-8">
                     <ul className="list-group">
                         {
-                            this.state.users.map(user => <li key={user.username} className="list-group-item list-group-item-action">{user.username}</li>)
+                            this.state.users.map( user => 
+                                <li key={user._id}  className="list-group-item list-group-item-action" onDoubleClick={() => this.deleteUser(user._id)}>
+                                    {user.username}
+                                </li>
+                            )
                         }
                     </ul>
                 </div>
