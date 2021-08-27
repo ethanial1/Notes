f3cb8bf4eb0beb5f48881eb434a843e8ca60d321
ethanial1 <camiloeiz00@gmail.com>
2021-08-27 11:05:06 -0500

    Crear nueva nota

--- frontend/src/components/CreateNoteForme.js
+++ frontend/src/components/CreateNoteForme.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default class CreateNoteForme extends Component {
+    state = {
+        users: [],
+        userSelected: '',
+        titulo: '',
+        descrip: '',
+        date: new Date()
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({users: res.data.map(user => user.username), userSelected: res.data[0].username});
+    }
+
+    onSumbit = async (e) => {
+        e.preventDefault();
+        const newNote = {
+            title: this.state.titulo,
+            descrip: this.state.descrip,
+            author: this.state.userSelected,
+            date: this.state.date
+        }
+
+        await axios.post('http://localhost:4000/api/notes', newNote);
+
+    }
+
+    onInputChange = e => {
+        this.setState(
+            {
+                //userSelected: e.target.value
+                [e.target.name] : e.target.value
+            }
+        );
+    }
+
+    onChanegeDate = date => {
+        this.setState({date});
+    }
     render() {
         return (
-            <div>
-                create Notes
+            <div className="col-md-6 offset-md-3">
+                <div className="card card-body">
+                    <h5>Crear nueva nota</h5>
+                    {/** SELECT USER  */}
+                    <div className="form-group">
+                        <select className="form-control" name="userSelected" onChange={this.onInputChange}>
+                            {this.state.users.map(urs => 
+                                <option key={urs.username} value={urs.username}>
+                                    {urs.username}
+                                </option>
+                            )}
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-group" placeholder="Titulo" name="titulo" onChange={this.onInputChange} required/>
+                    </div>
+                    <div className="form-group">
+                        <textarea name="descrip" className="form-control" placeholder="Descripción" onChange={this.onInputChange} required></textarea>
+                    </div>
+                    <div className="form-group">
+                        <DatePicker className="form-control" selected={this.state.date} onChange={this.onChanegeDate}/>
+                    </div>
+                    <form onSubmit={this.onSumbit}>
+
+                        <button type="submit" className="btn btn-primary">Save</button>
+                    </form>
+                </div>
             </div>
         )
     }
